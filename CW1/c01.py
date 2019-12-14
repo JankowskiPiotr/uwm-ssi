898fd291df7c3ae16a6b17bcad941a1814a14138
JankowskiPiotr <jankowski.piotr@o2.pl>
2019-12-14 19:55:31 +0100

    Wyszukanie i wyswietlenie min/max atrybutów

--- CW1/c01.py
+++ CW1/c01.py
@@ -5,10 +5,47 @@ class SystemDecyzyjny:
         f = open(self)
         print(f.read())
 
+    def getNumericAttributes(self):
+        i = 0
+        array = []
+        for elem in self:
+            if "n" in elem:
+                array.append(i)
+            i += 1
+        return array
+
+    def minAttributeValue(self, numericAttributes):
+        for i in numericAttributes:
+            array = []
+            for x in range(len(self)):
+                array.append(self[x][i])
+            print("attribute", i, "min value", min(array))
+
+    def maxAttributeValue(self, numericAttributes):
+        for i in numericAttributes:
+            array = []
+            for x in range(len(self)):
+                array.append(self[x][i])
+            print("attribute", i, "max value", max(array))
+
+    def splitIntoLines(self):
+        return re.split(r'\n', self)
+
+    def listAttributesAndTheirNumbers(self):
+        lines = SystemDecyzyjny.splitIntoLines(self)
+        myArray = []
+        for line in lines:
+            myArray.append(line.split(" "))
+        return myArray
+
+
 def main():
     SystemDecyzyjny.printFile("australian-type.txt")
-
-
+    array = SystemDecyzyjny.splitIntoLines(open("australian-type.txt").read())
+    print(array)
+    lines = SystemDecyzyjny.listAttributesAndTheirNumbers(open("australian.txt").read())
+    SystemDecyzyjny.minAttributeValue(lines, SystemDecyzyjny.getNumericAttributes(array))
+    SystemDecyzyjny.maxAttributeValue(lines, SystemDecyzyjny.getNumericAttributes(array))
 
 
 if __name__ == "__main__":
